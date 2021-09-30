22fec9a759700752650aad20f00f22c6ff6be3fe
jhuang43 <71346325+jhuang43@users.noreply.github.com>
2021-09-30 16:55:00 -0400

    Form timeseries with date, complaint type, and count

--- 2_data_aggregation.py
+++ 2_data_aggregation.py
@@ -1,4 +1,6 @@
 import pandas as pd
+
+# Read the csv file
 results = pd.read_csv('./data/raw.csv')
 
 # Create an empty DataFrame for the new timeseries
@@ -9,3 +11,16 @@ timeseries['created_date_hour'] = pd.to_datetime(results['created_date']).dt.str
 
 # Transfer over the complaint types
 timeseries['complaint_type'] = results['complaint_type']
+
+# Group over created_date_hour and complaint_type and count and insert as a new column
+timeseries['count'] = timeseries.groupby(by=['created_date_hour', 'complaint_type'])['complaint_type'].transform('count')
+
+# Drop duplicates
+timeseries = pd.DataFrame.drop_duplicates(timeseries)
+
+# # Checking that the count matches with the original number of records
+# results['unique_key'].count()
+# timeseries['count'].sum()
+
+# Convert and download as "timeseries.csv" to the data folder without the index
+timeseries.to_csv('./data/timeseries.csv', index = False)
