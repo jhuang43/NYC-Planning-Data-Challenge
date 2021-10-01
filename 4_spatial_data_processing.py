f2dd3d84c82e918fc582065c72e20d363b0239a4
jhuang43 <71346325+jhuang43@users.noreply.github.com>
2021-10-01 16:16:07 -0400

    Spatial Join done for choropleth

--- 4_spatial_data_processing.py
+++ 4_spatial_data_processing.py
@@ -11,15 +11,27 @@ from matplotlib import pyplot as plt
 plt.close('all')
 
 # Retrieve geodata of the 2020 Neighborhood Tabulation Areas (NTAs) and convert to json
+# Retrieve geodata of the 2020 Neighborhood Tabulation Areas (NTAs)
 url = "https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/NYC_Neighborhood_Tabulation_Areas_2020/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=pgeojson"
 geodata = gpd.read_file(url)
 
 # Read the raw.csv file 
 raw = pd.read_csv('./data/raw.csv')
 
+# Selecting all rows with a specific complaint_type
+raw = raw.loc[raw['complaint_type'] == 'UNSANITARY CONDITION']
+raw.reset_index(drop=True)
 
+# Converting raw into a geoDataFrame 
+# Creating geometry column comprised of longitude and latitude
+# Setting crs to match geodata's crs
+gpd_raw = gpd.GeoDataFrame(raw, geometry=gpd.points_from_xy(raw['longitude'], raw['latitude'], crs=geodata.crs))
 
+# gpd_raw.plot()
+# plt.show(block=False)
 
+# select/confirm that all the points in gpd_raw's geometry are within the geodata's geometry and merge
+merge = gpd.sjoin(gpd_raw, geodata, how="inner", op="within")
 
-
-plt.show(block=False)
+# merge.plot()
+# plt.show(block=False)
