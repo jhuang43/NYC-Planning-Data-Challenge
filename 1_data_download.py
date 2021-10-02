186acbe890b3bc9fa21553ab23d2cc6cf2d0d3cb
jhuang43 <71346325+jhuang43@users.noreply.github.com>
2021-10-01 23:52:37 -0400

    Update 1_data_download.py

--- 1_data_download.py
+++ 1_data_download.py
@@ -9,6 +9,29 @@ from sodapy import Socrata
 from datetime import datetime, timedelta
 import os
 
+def save_as_csv(file_df, name='file'):
+    """Saves DataFrame as a csv file.
+
+    If the argument `name` isn't passed in, the file's name will be 'file.csv'.
+
+    Parameters
+    ----------
+    file_df : DataFrame
+    The DataFrame that will be converted to csv
+
+    name : str, optional
+    The name the csv will be saved as
+    """
+
+    # Check to see if a data folder exists, if not, make a folder named "data"
+    if not os.path.exists('./data'): 
+        os.mkdir('./data')
+
+    # Convert and download as csv to the data folder without the index    
+    file_df.to_csv(f'./data/{name}.csv', index=False)
+
+
+
 # Unauthenticated client only works with public data sets. Note 'None' in place of application token, and no username or password:
 client = Socrata("data.cityofnewyork.us", None)
 
@@ -34,9 +57,4 @@ results_df = pd.DataFrame.from_records(results)
 # results_df['created_date'].max()
 # results_df['agency'].value_counts()
 
-# Check to see if a data folder exists, if not, make a folder named "data"
-if not os.path.exists('./data'):
-    os.mkdir('./data')
-
-# Convert and download as "raw.csv" to the data folder without the index
-results_df.to_csv('./data/raw.csv', index=False)
+save_as_csv(results_df, 'raw')
